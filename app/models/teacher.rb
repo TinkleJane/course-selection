536e16fa20414906f948a1f10c0fd5c55c8d7937
tinklexjane <tinklexjane@gmail.com>
2017-04-11 10:49:21 +0800

    Spec 3

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,4 +1,5 @@
 class Teacher < ApplicationRecord
+  has_many :teacher_relationships
   has_many :courses
-  has_many :students
+  has_many :students, through: :teacher_relationships, source: :student
 end
